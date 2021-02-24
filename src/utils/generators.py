3ae2bc605e82803f054354321f2a5306a8b7fc1e
Denis <ecodenis@yandex.ru>
2021-02-24 22:02:42 +0100

    generators.py file was added. It represents generators inherited by tf.keras.utils.Sequence. Now, it is just defined with empty functions.

---
+++ src/utils/generators.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import numpy as np
+import pandas as pd
+import os
+import tensorflow as tf
+
+__author__ = "Denis Dresvyanskiy"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Denis Dresvyanskiy"]
+__maintainer__ = "Denis Dresvyanskiy"
+__email__ = "denis.dresvyanskiy@uni-ulm.de"
+
+class FixedChunksGenerator(tf.keras.utils.Sequence):
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def on_epoch_end(self):
+        pass
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    pass

7f8603d04562ff4a93e4043eed69c9be214ea7c0
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-03-09 15:40:30 +0100

    EES_challenge.py : some code to allow using AudioFixedChunksGenerator with 'data' mode were added.

--- src/EES_challenge.py
+++ src/EES_challenge.py
@@ -1,11 +1,13 @@
+import gc
 import os
-from typing import Dict
+from typing import Dict, Tuple
 
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn.utils import class_weight
-
+import scipy.signal as sps
+from src.utils.audio_preprocessing_utils import load_wav_file
 from src.utils.generators import AudioFixedChunksGenerator
 from src.utils.tf_utils import chunk_based_rnn_model
 
@@ -17,45 +19,64 @@ def read_labels(path:str) -> Dict[str, np.ndarray]:
         result_dict[row['filename']]=np.array(row['label']).reshape((1,1))
     return result_dict
 
+def load_all_wav_files(path:str, resample:bool=False, new_sample_rate:int=16000)-> Dict[str,Tuple[np.ndarray, int]]:
+    loaded_wav_files={}
+    filenames=os.listdir(path)
+    for filename in filenames:
+        sample_rate,wav_file=load_wav_file(os.path.join(path,filename))
+        # Resample data
+        if resample and sample_rate!=new_sample_rate:
+            number_of_samples = round(len(wav_file) * new_sample_rate / sample_rate)
+            wav_file = sps.resample(wav_file, number_of_samples)
+            sample_rate=new_sample_rate
 
-
+        if len(wav_file.shape)<2: wav_file=wav_file[..., np.newaxis]
+        loaded_wav_files[filename.split(".")[0]]=(wav_file, sample_rate)
+    return loaded_wav_files
 
 
 if __name__ == '__main__':
-    path_to_train_data='E:\\Databases\\Compare_2021_ESS\\wav\\train\\'
-    path_to_train_labels='E:\\Databases\\Compare_2021_ESS\\lab\\train.csv'
-    path_to_devel_labels='E:\\Databases\\Compare_2021_ESS\\lab\\devel.csv'
-    path_to_devel_data = 'E:\\Databases\\Compare_2021_ESS\\wav\\dev\\'
+    path_to_train_data='D:\\Databases\\Compare_2021_ESS\\wav\\train\\'
+    path_to_train_labels='D:\\Databases\\Compare_2021_ESS\\lab\\train.csv'
+    path_to_devel_labels='D:\\Databases\\Compare_2021_ESS\\lab\\devel.csv'
+    path_to_devel_data = 'D:\\Databases\\Compare_2021_ESS\\wav\\dev\\'
     # params
     sequence_max_length = 12
     window_length = 0.5
     num_chunks = int(sequence_max_length / window_length)
     num_classes=3
-    data_preprocessing_mode = 'EGEMAPS'
+    data_preprocessing_mode = 'LLD'
     label_type = 'sequence_to_one'
     batch_size = 16
     num_mfcc=128
 
     # train data
     train_labels=read_labels(path_to_train_labels)
+    train_data=load_all_wav_files(path_to_train_data, resample=True)
     train_generator = AudioFixedChunksGenerator(sequence_max_length=sequence_max_length, window_length=window_length,
-                                          load_mode='path',
-                                          load_path=path_to_train_data,
+                                          load_mode='data',
+                                          data=train_data,
                                           data_preprocessing_mode=data_preprocessing_mode, num_mfcc=num_mfcc,
                                           labels=train_labels, labels_type='sequence_to_one', batch_size=batch_size,
                                           normalization=True, one_hot_labeling=True, num_classes=num_classes,
-                                          subwindow_size=0.25, subwindow_step=0.1)
+                                          subwindow_size=0.1, subwindow_step=0.05)
+    for x,y in train_generator:
+        print(x,y)
 
+    del train_data
+    gc.collect()
     # validation data
     devel_labels=read_labels(path_to_devel_labels)
+    val_data=load_all_wav_files(path_to_devel_data, resample=True)
     devel_generator = AudioFixedChunksGenerator(sequence_max_length=sequence_max_length, window_length=window_length,
-                                          load_mode='path',
-                                          load_path=path_to_devel_data,
+                                          load_mode='data',
+                                          data=val_data,
                                           data_preprocessing_mode=data_preprocessing_mode,num_mfcc=num_mfcc,
                                           labels=devel_labels, labels_type='sequence_to_one', batch_size=batch_size,
                                           normalization=True, one_hot_labeling=True, num_classes=num_classes)
-
-    model=chunk_based_rnn_model(input_shape=(num_chunks,32,128), num_output_neurons=num_classes,
+    del val_data
+    gc.collect()
+    model=chunk_based_rnn_model(input_shape=(num_chunks,22,260), num_output_neurons=num_classes,
     neurons_on_layer = (256, 256), rnn_type = 'LSTM',
                                 need_regularization=True, dropout=True)
     model.summary()

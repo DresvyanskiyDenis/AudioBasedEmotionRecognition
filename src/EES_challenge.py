generators.py and EES_challenge.py : the bug with different metric score after shuffling the data array was fixed. Now, the normalization is applied only once in the __init__() function of generator. The predictions after training on validation set now are also written. sequence_to_one_models.py : 1d CNN + attention model was a little bit changed.

src/EES_challenge.py
@@ -46,7 +46,7 @@ def test_different_features(feature_types:Tuple[str,...], sequence_max_length:fl
     # params
     num_chunks = int(sequence_max_length / window_length)
     label_type = 'sequence_to_one'
-    batch_size = 16
+    batch_size = 4
     num_mfcc = 128
     # check if directory where results will be saved exists
     if not os.path.exists(path_to_save_results):
@@ -89,7 +89,8 @@ def test_different_features(feature_types:Tuple[str,...], sequence_max_length:fl
                                                         num_mfcc=num_mfcc,
                                                         labels=devel_labels, labels_type=label_type,
                                                         batch_size=batch_size,
-                                                        normalization=True, one_hot_labeling=True,
+                                                        normalization=True, normalizer=train_generator.normalizer,
+                                                        one_hot_labeling=True,
                                                         num_classes=num_classes,
                                                         subwindow_size=subwindow_size, subwindow_step=subwindow_step,
                                                         precutting=False, precutting_window_size=None,
@@ -103,8 +104,18 @@ def test_different_features(feature_types:Tuple[str,...], sequence_max_length:fl
         model = chunk_based_rnn_model(input_shape=input_shape, num_output_neurons=num_classes,
                                       neurons_on_layer=(256, 256), rnn_type='LSTM', bidirectional=False,
                                       need_regularization=True, dropout=True)
+        """model=chunk_based_1d_cnn_attention_model(input_shape=input_shape, num_output_neurons=num_classes,
+                                       filters_per_layer = (128, 128, 128, 256,256),
+                                       filter_sizes = (20, 15, 10, 6, 5),
+                                       pooling_sizes = (8, 4,2,2,2),
+                                       pooling_step = 1,
+                                       need_regularization= True,
+                                       dropout= True,
+                                       dropout_rate=0.3
+                                       )"""
         model.summary()
-        model.compile(optimizer=tf.keras.optimizers.Adam(0.0002), loss='categorical_crossentropy',
+        #model.load_weights('C:\\Users\\Denis\\PycharmProjects\\AudioBasedEmotionRecognition\\src\\results\\MFCC_subwindow_3.000000_0.100000\\model_weights.h5', by_name=True)
+        model.compile(optimizer=tf.keras.optimizers.RMSprop(0.0005, decay=1e-6), loss='categorical_crossentropy',
                       metrics=[tf.keras.metrics.Recall()])
         # compute class weights
         #train_labels = pd.read_csv(path_to_train_labels)
@@ -113,18 +124,27 @@ def test_different_features(feature_types:Tuple[str,...], sequence_max_length:fl
         #                                                  train_labels['label'].values)
         #class_weights = {i: class_weights[i] for i in range(num_classes)}
         # train model
-        hist=model.fit(train_generator, epochs=100,
+        hist=model.fit(train_generator, epochs=50,
                        callbacks=[validation_callback(devel_generator)])
         # collect the best reached score
         macro_recall=custom_recall_validation_with_generator(devel_generator, model)
         results.append((feature_type+'_macro_recall', macro_recall))
         print('feature_type:%s, max_val_MACRO_recall:%f' % (feature_type, macro_recall))
+        # save predictions
+        predictions=devel_generator.get_dict_predictions(model)
+        df_for_saving_predictions=pd.DataFrame(data=np.zeros((len(predictions), num_classes + 1)))
+        df_for_saving_predictions.iloc[:,0]=np.array(list(predictions.keys()))
+        df_for_saving_predictions.iloc[:,1:]=np.array(list(predictions.values())).reshape((len(predictions), num_classes))
+        df_for_saving_predictions.columns=['filename']+['class_prob_%i'%i for i in range(num_classes)]
+        df_for_saving_predictions.to_csv(path_to_save_model+'predictions.csv', index=False)
+        # save final results and weights
         pd.DataFrame(results, columns=['feature_type', 'val_recall']).to_csv(
             os.path.join(path_to_save_results, 'results_subwindow_%f_%f.csv' % (subwindow_size, subwindow_step)), index=False)
         model.save_weights(os.path.join(path_to_save_model, 'model_weights.h5'))
+
         # clear RAM
         del model
-        del hist
+        #del hist
         del train_generator
         del devel_generator
         gc.collect()
@@ -139,7 +159,7 @@ def custom_recall_validation_with_generator(generator:ChunksGenerator_preprocess
         predictions=predictions.argmax(axis=-1).reshape((-1,))
         total_predictions=np.append(total_predictions,predictions)
         total_ground_truth=np.append(total_ground_truth,y.argmax(axis=-1).reshape((-1,)))
-    return recall_score(total_ground_truth, total_predictions,average='macro' )
+    return recall_score(total_ground_truth, total_predictions,average='macro')
 
 class validation_callback(tf.keras.callbacks.Callback):
     def __init__(self, val_generator:ChunksGenerator_preprocessing):
@@ -168,10 +188,10 @@ if __name__ == '__main__':
     # params
     sequence_max_length = 12
     window_length = 0.5
-    subwindow_lengths=(0.2, 0.3, 0.4)
-    subwindow_steps=(0.1, 0.2)
+    subwindow_lengths=(0.2,0.3)
+    subwindow_steps=(0.1,0.2)
     for subwindow_length in subwindow_lengths:
         for subwindow_step in subwindow_steps:
-            test_different_features(feature_types=('HLD', 'EGEMAPS', 'HLD_EGEMAPS'),
+            test_different_features(feature_types=('MFCC',),
                                     sequence_max_length=sequence_max_length, window_length=window_length,
                                     subwindow_size=subwindow_length,subwindow_step=subwindow_step)

src/utils/generators.py
@@ -60,8 +60,8 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
     def __init__(self, *, sequence_max_length: float, window_length: float, data: Optional[Data_type_format] = None,
                  data_preprocessing_mode: Optional[str] = 'raw', num_mfcc: Optional[int] = 128,
                  labels: Dict[str, np.ndarray] = None, labels_type: str = 'sequence_to_one', batch_size: int = 32,
-                 normalization: bool = False, one_hot_labeling: Optional[bool] = None,
-                 num_classes: Optional[int] = None,
+                 normalization: bool = False, normalizer:Optional[object]=None,
+                 one_hot_labeling: Optional[bool] = None, num_classes: Optional[int] = None,
                  subwindow_size: Optional[float] = None, subwindow_step: Optional[float] = None,
                  precutting: bool = False,
                  precutting_window_size: Optional[float] = None, precutting_window_step: Optional[float] = None):
@@ -85,6 +85,10 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
 
         :param normalization: bool
                     Apply normalization to the features before yielding or not.
+        :param normalizer: object
+                    if not None, the provided normalizer will be applied to transform data with the help
+                    of .transform() function. These objects should be sklearn.preprocessing objects.
+                    like: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
         :param one_hot_labeling: Optional[bool]
                     Apply one hot labeling to the output labels or not.
         :param num_classes: Optional[int]
@@ -116,6 +120,7 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
         self.batch_size = batch_size
         self.num_mfcc = num_mfcc
         self.normalization = normalization
+        self.normalizer=normalizer
         self.one_hot_labeling = one_hot_labeling
         self.num_classes = num_classes
         self.subwindow_size = subwindow_size
@@ -193,11 +198,9 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
                 the batch of x,y pairs
         """
         data, labels = self._form_batch_with_data_load_mode(index)
-        if self.normalization:
-            data = self.normalize_batch_of_chunks(data)
         if self.one_hot_labeling:
             labels = tf.keras.utils.to_categorical(labels, num_classes=self.num_classes)
-        return data, labels
+        return data.astype('float32'), labels
 
     def on_epoch_end(self):
         """Do some actions at the end of epoch.
@@ -206,15 +209,16 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
         """
         random.shuffle(self.indexes)
 
+    """
     def normalize_batch_of_chunks(self, batch_of_chunks: np.ndarray) -> np.ndarray:
-        """Normalizes a batch of chunks via StandardScaler() normalizer.
+        \"""Normalizes a batch of chunks via StandardScaler() normalizer.
         https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
 
         :param batch_of_chunks: np.ndarray
                     ndarray with shape (batch_size, num_chunks, window_size, num_features)
         :return: np.ndarray
                     ndarray with the same shape as batch_of_chunks
-        """
+        \"""
         # reshape array to 2D (window_num, num_features)
         array_to_normalize = batch_of_chunks.reshape((-1, batch_of_chunks.shape[-1]))
         # create scaler
@@ -224,6 +228,7 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
         # reshape obtained data back to initial shape
         result_array = array_to_normalize.reshape(batch_of_chunks.shape)
         return result_array
+        """
 
     def _form_batch_with_data_load_mode(self, index: int) -> Tuple[np.ndarray, np.ndarray]:
         """Forms batch, which consists of data and labels chosen according index from shuffled self.indexes.
@@ -362,10 +367,33 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
             cut_sequence, sample_rate = self.data[filename]
             self.data[filename] = (extracted_features, sample_rate)
 
-        '''for key, value in self.data.items():
-            cut_sequence, sample_rate = value
-            cut_sequence = self._preprocess_cut_audio(cut_sequence, sample_rate, self.data_preprocessing_mode, self.num_mfcc)
-            self.data[key]=(cut_sequence, sample_rate)'''
+        if self.normalization:
+            if self.normalizer is None:
+                self.data, self.normalizer= self._normalize_all_data(self.data, return_normalizer=True)
+            else:
+                self.data = self._normalize_all_data(self.data, normalizer=self.normalizer)
+
+    def _normalize_all_data(self, data:Data_type_format, return_normalizer:bool=False,
+                        normalizer:Optional[object]=None) -> Union[Data_type_format, Tuple[Data_type_format, object]]:
+        if normalizer is None:
+            normalizer=StandardScaler()
+            concatenated_data = []
+            for key, item in data.items():
+                array, sample_rate = item
+                concatenated_data.append(array)
+            concatenated_data = np.concatenate(concatenated_data, axis=0)
+            normalizer=normalizer.fit(concatenated_data.reshape((-1, concatenated_data.shape[-1])))
+            del concatenated_data
+        for key, item in data.items():
+            array, sample_rate = item
+            old_shape=array.shape
+            array=array.reshape((-1, array.shape[-1]))
+            array=normalizer.transform(array)
+            array=array.reshape(old_shape)
+            data[key]=(array, sample_rate)
+        if return_normalizer:
+            return data, normalizer
+        return data
 
     def _form_indexes(self) -> List[Union[int, Tuple[str, int]]]:
         """Forms random indexes depend on data type was loaded.
@@ -469,6 +497,14 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
             data[key] = (array, sample_rate)
         return data
 
+    def get_dict_predictions(self, model:tf.keras.Model):
+        predictions={}
+        for key, item in self.data.items():
+            array, sample_rate = item
+            prediction=model.predict(array)
+            predictions[key]=prediction
+        return predictions
+
     def __get_features_shape__(self):
         """Provides the shape of features, e.g
         (num_chunks, window_size, num_features) will give (window_size, num_features), since it is the features
@@ -481,6 +517,8 @@ class ChunksGenerator_preprocessing(tf.keras.utils.Sequence):
         key = list(self.data.keys())[0]
         # return shape of data
         return self.data[key][0].shape
+
+
 
 
 if __name__ == "__main__":

src/utils/sequence_to_one_models.py
@@ -355,18 +355,19 @@ def chunk_based_1d_cnn_attention_model(*, input_shape: Tuple[int, ...], num_outp
         if dropout: x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(dropout_rate))(x)
         # if now is a step of pooling layer, then create and add to model AveragePooling layer
         if (layer_idx + 1) % pooling_step == 0:
-            x = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool1D(pooling_sizes[pooling_idx]))(x)
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.AveragePooling1D(pooling_sizes[pooling_idx]))(x)
             pooling_idx += 1
-    #  average the last hidden states from different chunks with the help of 1x1 Conv2d
-    x = tf.keras.layers.Conv2D(32, 1, activation='tanh')(x)
-    if dropout: x = tf.keras.layers.Dropout(dropout_rate)(x)
-    x = tf.keras.layers.Conv2D(1, 1, activation='tanh')(x)
-    # squeeze the last dimension
-    x = tf.keras.layers.Reshape((x.shape[1:-1]))(x)
+    #  average pooling to squeeze tensor to 2-d
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.GlobalAveragePooling1D())(x)
     # attention
-    x = Attention(11)(x)
+    x = Attention(128)(x)
+    #x = tf.keras.layers.LSTM(128, return_sequences=True)(x)
+    #if dropout: x=tf.keras.layers.Dropout(dropout_rate)(x)
+    #x = tf.keras.layers.LSTM(128, return_sequences=False)(x)
+    #if dropout: x = tf.keras.layers.Dropout(dropout_rate)(x)
     # output
-    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
     output = tf.keras.layers.Dense(num_output_neurons, activation='softmax')(x)
     model = tf.keras.Model(inputs=[input], outputs=[output])
     return model
@@ -411,7 +412,13 @@ def CCC_loss_tf(y_true, y_pred):
 
 
 if __name__ == "__main__":
-    model=chunk_based_rnn_attention_model(input_shape=(12,20,125), num_output_neurons=3,
-                                    neurons_on_rnn_layer=(128, 256), rnn_type='LSTM', bidirectional=False,
-                                    need_regularization=True, dropout=True)
+    model=chunk_based_1d_cnn_attention_model(input_shape=(24,8000,1), num_output_neurons=3,
+                                       filters_per_layer = (64, 128, 128, 256,256),
+                                       filter_sizes = (12, 10, 8, 6, 5),
+                                       pooling_sizes = (8, 4,2,2,2),
+                                       pooling_step = 1,
+                                       need_regularization= True,
+                                       dropout= True,
+                                       dropout_rate=0.3
+                                       )
     model.summary()
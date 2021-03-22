0210a21ef81bb4ff55143a36c473275e1e93bac3
Denis <ecodenis@yandex.ru>
2021-03-22 22:07:13 +0100

    Two new functions to extract functionals from provided 2-dimensional numpy array (window) was added. Currently, it supports mean, std, 1- and 2-order polynomial coefficients.

--- src/utils/audio_preprocessing_utils.py
+++ src/utils/audio_preprocessing_utils.py
@@ -22,10 +22,12 @@ List of functions:
     extracting cut sequence on subwindows and extracts defined features within these subwindows. Extracted features then
      combine (concatenate) across feature axis. Currently the supported features are ('EGEMAPS', 'HLD')
     * cut_data_on_chunks - cut provided sequence on chunks (windows). The data can be either 1-d or 2-d np.ndarray.
+    TODO: add new functions to list
 """
 
 from typing import List, Union, Tuple, Optional
 import numpy as np
+from numpy import polynomial
 from scipy.io import wavfile
 import librosa
 import opensmile
@@ -37,7 +39,7 @@ __maintainer__ = "Denis Dresvyanskiy"
 __email__ = "denis.dresvyanskiy@uni-ulm.de"
 
 
-def load_wav_file(path:str) -> Tuple[int,np.ndarray]:
+def load_wav_file(path: str) -> Tuple[int, np.ndarray]:
     """Loads .wav file according provided path and returns sample_rate of audio and the data
 
     :param path: str
@@ -49,8 +51,8 @@ def load_wav_file(path:str) -> Tuple[int,np.ndarray]:
     return sample_rate, data
 
 
-def extract_mfcc_from_audio_sequence(data:np.ndarray, sample_rate:int, num_mfcc:int,
-                                     length_fft:int=512, length_fft_step:int=256) -> np.ndarray:
+def extract_mfcc_from_audio_sequence(data: np.ndarray, sample_rate: int, num_mfcc: int,
+                                     length_fft: int = 512, length_fft_step: int = 256) -> np.ndarray:
     """Extracts features from given data ndarray with the help of librosa library.
     https://librosa.org/doc/main/generated/librosa.feature.mfcc.html
 
@@ -70,14 +72,15 @@ def extract_mfcc_from_audio_sequence(data:np.ndarray, sample_rate:int, num_mfcc:
                 the first dimension is computed by librosa and depends on length_fft and length_fft_step
     """
     # check if data is monotonic, but have 2 dimensions
-    if len(data.shape)==2 and data.shape[1]==1:
-        data=data.reshape((-1,))
-    mfcc_features=librosa.feature.mfcc(data, sr=sample_rate, n_mfcc=num_mfcc,
-                                       n_fft=length_fft, hop_length=length_fft_step)
+    if len(data.shape) == 2 and data.shape[1] == 1:
+        data = data.reshape((-1,))
+    mfcc_features = librosa.feature.mfcc(data, sr=sample_rate, n_mfcc=num_mfcc,
+                                         n_fft=length_fft, hop_length=length_fft_step)
     return mfcc_features.T
 
-def extract_opensmile_features_from_audio_sequence(data:Union[np.ndarray, str], sample_rate:Optional[int]=None,
-                                                   feature_type:str='LLD') -> np.ndarray:
+
+def extract_opensmile_features_from_audio_sequence(data: Union[np.ndarray, str], sample_rate: Optional[int] = None,
+                                                   feature_type: str = 'LLD') -> np.ndarray:
     """Extracts opensmile ComParE_2016 features from audio sequence represented either by ndarray or path.
     https://github.com/audeering/opensmile-python
 
@@ -88,18 +91,18 @@ def extract_opensmile_features_from_audio_sequence(data:Union[np.ndarray, str], 
     :return: np.ndarray
                 extracted features
     """
-    supported_feature_types=('LLD', 'Compare_2016_functionals','EGEMAPS')
+    supported_feature_types = ('LLD', 'Compare_2016_functionals', 'EGEMAPS')
     if not feature_type in supported_feature_types:
-        raise AttributeError('feature_type must the value from %s. Got %s.' % (supported_feature_types,feature_type))
+        raise AttributeError('feature_type must the value from %s. Got %s.' % (supported_feature_types, feature_type))
     # configure opensmile to extract desirable features
     if feature_type == 'LLD':
-        feature_level=opensmile.FeatureLevel.LowLevelDescriptors
-        feature_set=opensmile.FeatureSet.ComParE_2016
+        feature_level = opensmile.FeatureLevel.LowLevelDescriptors
+        feature_set = opensmile.FeatureSet.ComParE_2016
     elif feature_type == 'Functionals':
-        feature_level=opensmile.FeatureLevel.Functionals
+        feature_level = opensmile.FeatureLevel.Functionals
         feature_set = opensmile.FeatureSet.ComParE_2016
     elif feature_type == 'EGEMAPS':
-        feature_level=opensmile.FeatureLevel.Functionals
+        feature_level = opensmile.FeatureLevel.Functionals
         feature_set = opensmile.FeatureSet.eGeMAPSv02
     # create opensmile Extractor
     smile = opensmile.Smile(
@@ -107,18 +110,20 @@ def extract_opensmile_features_from_audio_sequence(data:Union[np.ndarray, str], 
         feature_level=feature_level,
     )
     # check if data is a valid type and if yes, extract features properly
-    if isinstance(data,str):
+    if isinstance(data, str):
         extracted_features = smile.process_file(data).values
     elif isinstance(data, np.ndarray):
         # check if audio data is one-channel, then reshape it to 1D array (the requirement of opensmile)
-        if len(data.shape)==2 and data.shape[1]==1:
-            data=data.reshape((-1,))
+        if len(data.shape) == 2 and data.shape[1] == 1:
+            data = data.reshape((-1,))
         extracted_features = smile.process_signal(data, sampling_rate=sample_rate).values
     else:
-        raise AttributeError('Data should be either ndarray or str. Got %s.'%(type(data)))
+        raise AttributeError('Data should be either ndarray or str. Got %s.' % (type(data)))
     return extracted_features
 
-def extract_HLDs_from_LLDs(LLDs:np.ndarray, window_size:float, window_step:float, required_HLDs:Tuple[str,...]) -> np.ndarray:
+
+def extract_HLDs_from_LLDs(LLDs: np.ndarray, window_size: float, window_step: float,
+                           required_HLDs: Tuple[str, ...]) -> np.ndarray:
     """Extracts High level discriptors (functionals) from low-level discriptors. The output is in format List[np.ndarray] -
        each np.ndarray is functionals extracted from window calculated with the help of window_size and window_step params
 
@@ -137,21 +142,22 @@ def extract_HLDs_from_LLDs(LLDs:np.ndarray, window_size:float, window_step:float
     :return: List[np.ndarray]
             The list of np.ndarrays. Each np.ndarray corresponds to HLDs (functionals) extracted from window.
     """
-    supported_HLDs=('min', 'max', 'mean', 'std')
+    supported_HLDs = ('min', 'max', 'mean', 'std')
     # check if required_HLDs are all supported by this function
     if not set(required_HLDs).issubset(supported_HLDs):
-        raise AttributeError('Required HLDs contain some unsupported HLDs. Possible HLDs: %s. Got: %s.'%(supported_HLDs, required_HLDs))
+        raise AttributeError('Required HLDs contain some unsupported HLDs. Possible HLDs: %s. Got: %s.' % (
+        supported_HLDs, required_HLDs))
     # calculate sizes of step and window in units (indexes)
-    window_size_in_units=int(round(window_size*LLDs.shape[0]))
+    window_size_in_units = int(round(window_size * LLDs.shape[0]))
     window_step_in_units = int(round(window_step * LLDs.shape[0]))
     # cut LLDs on windows
-    cut_LLDs=cut_data_on_chunks(LLDs, window_size_in_units, window_step_in_units)
+    cut_LLDs = cut_data_on_chunks(LLDs, window_size_in_units, window_step_in_units)
     # extract HLDs for each window
-    result_array=[]
+    result_array = []
     for window_idx in range(len(cut_LLDs)):
-        calculated_HLDs=[]
+        calculated_HLDs = []
         for HLD_type in required_HLDs:
-            if HLD_type=='min':
+            if HLD_type == 'min':
                 calculated_HLDs.append(cut_LLDs[window_idx].min(axis=0))
             elif HLD_type == 'max':
                 calculated_HLDs.append(cut_LLDs[window_idx].max(axis=0))
@@ -159,13 +165,14 @@ def extract_HLDs_from_LLDs(LLDs:np.ndarray, window_size:float, window_step:float
                 calculated_HLDs.append(cut_LLDs[window_idx].mean(axis=0))
             elif HLD_type == 'std':
                 calculated_HLDs.append(cut_LLDs[window_idx].std(axis=0))
-        calculated_HLDs=np.concatenate(calculated_HLDs, axis=0)[np.newaxis,...]
+        calculated_HLDs = np.concatenate(calculated_HLDs, axis=0)[np.newaxis, ...]
         result_array.append(calculated_HLDs)
-    result_array=np.concatenate(result_array, axis=0)
+    result_array = np.concatenate(result_array, axis=0)
     return result_array
 
-def extract_subwindow_EGEMAPS_from_audio_sequence(sequence:np.ndarray, sample_rate:int,
-                                                      subwindow_size:float, subwindow_step:float) -> np.ndarray:
+
+def extract_subwindow_EGEMAPS_from_audio_sequence(sequence: np.ndarray, sample_rate: int,
+                                                  subwindow_size: float, subwindow_step: float) -> np.ndarray:
     """
 
     :param sequence: np.ndarray
@@ -191,15 +198,17 @@ def extract_subwindow_EGEMAPS_from_audio_sequence(sequence:np.ndarray, sample_ra
     # extract HLDs for each window
     result_array = []
     for subwindow_idx in range(len(cut_sequence)):
-        calculated_subwindow_egemaps=extract_opensmile_features_from_audio_sequence(cut_sequence[subwindow_idx],
-                                                                                    sample_rate, feature_type='EGEMAPS')
+        calculated_subwindow_egemaps = extract_opensmile_features_from_audio_sequence(cut_sequence[subwindow_idx],
+                                                                                      sample_rate,
+                                                                                      feature_type='EGEMAPS')
         result_array.append(calculated_subwindow_egemaps)
-    result_array=np.concatenate(result_array, axis=0)
+    result_array = np.concatenate(result_array, axis=0)
     return result_array
 
-def extract_combined_features_with_sibwindows(sequence:np.ndarray, sample_rate:int,
-                                              subwindow_size:float, subwindow_step:float,
-                                              feature_types:Tuple[str,...]) -> np.ndarray:
+
+def extract_combined_features_with_sibwindows(sequence: np.ndarray, sample_rate: int,
+                                              subwindow_size: float, subwindow_step: float,
+                                              feature_types: Tuple[str, ...]) -> np.ndarray:
     """Extracts defined in feature_types features from audio. Before extracting, the audio sequence will be
     cut on slices (subwindows) according to subwindow_size and subwindow_step.
     Currently supported_features=('EGEMAPS','HLD')
@@ -217,7 +226,7 @@ def extract_combined_features_with_sibwindows(sequence:np.ndarray, sample_rate:i
     :return: np.ndarray
                 Extracted and concatenated within subwindows features
     """
-    supported_features=('EGEMAPS','HLD')
+    supported_features = ('EGEMAPS', 'HLD')
     # check if feature_types contains all supported features
     if not set(feature_types).issubset(supported_features):
         raise AttributeError('Requested feature_types contain some unsupported features. Supported features: %s. '
@@ -229,25 +238,26 @@ def extract_combined_features_with_sibwindows(sequence:np.ndarray, sample_rate:i
     cut_sequence = cut_data_on_chunks(sequence, window_size_in_units, window_step_in_units)
     result_array = []
     for subwindow_idx in range(len(cut_sequence)):
-        extracted_features=[]
+        extracted_features = []
         for feature_type in feature_types:
-            if feature_type=='HLD':
-                lld=extract_opensmile_features_from_audio_sequence(cut_sequence[subwindow_idx], sample_rate, 'LLD')
-                hld=extract_HLDs_from_LLDs(lld, window_size=1,
-                                                        window_step=1,
-                                                        required_HLDs=('min', 'max', 'mean', 'std'))
+            if feature_type == 'HLD':
+                lld = extract_opensmile_features_from_audio_sequence(cut_sequence[subwindow_idx], sample_rate, 'LLD')
+                hld = extract_HLDs_from_LLDs(lld, window_size=1,
+                                             window_step=1,
+                                             required_HLDs=('min', 'max', 'mean', 'std'))
                 extracted_features.append(hld)
-            elif feature_type=='EGEMAPS':
+            elif feature_type == 'EGEMAPS':
                 egemaps = extract_opensmile_features_from_audio_sequence(cut_sequence[subwindow_idx],
-                                                                                      sample_rate,
-                                                                                      feature_type='EGEMAPS')
+                                                                         sample_rate,
+                                                                         feature_type='EGEMAPS')
                 extracted_features.append(egemaps)
-        extracted_features=np.concatenate(extracted_features, axis=-1)
+        extracted_features = np.concatenate(extracted_features, axis=-1)
         result_array.append(extracted_features)
     result_array = np.concatenate(result_array, axis=0)
     return result_array
 
-def cut_data_on_chunks(data:np.ndarray, chunk_length:int, window_step:int) -> List[np.ndarray]:
+
+def cut_data_on_chunks(data: np.ndarray, chunk_length: int, window_step: int) -> List[np.ndarray]:
     """Cuts data on chunks according to supplied chunk_length and windows_step.
         Example:
         data=|123456789|, chunk_length=4, window_step=3
@@ -262,34 +272,79 @@ def cut_data_on_chunks(data:np.ndarray, chunk_length:int, window_step:int) -> Li
     :return: list of np.ndarrays
                 cut on windows data
     """
-    if data.shape[0]<chunk_length:
-        raise AttributeError("data length should be >= chunk length. Got data length:%i, chunk length:%i"%(data.shape[0], chunk_length))
-    if data.shape[0]<window_step:
-        raise AttributeError("data length should be >= window_step. Got data length:%i, window_step:%i"%(data.shape[0], window_step))
+    if data.shape[0] < chunk_length:
+        raise AttributeError("data length should be >= chunk length. Got data length:%i, chunk length:%i" % (
+        data.shape[0], chunk_length))
+    if data.shape[0] < window_step:
+        raise AttributeError(
+            "data length should be >= window_step. Got data length:%i, window_step:%i" % (data.shape[0], window_step))
 
-    cut_data=[]
-    num_chunks=int(np.ceil((data.shape[0]-chunk_length)/window_step+1))
+    cut_data = []
+    num_chunks = int(np.ceil((data.shape[0] - chunk_length) / window_step + 1))
 
-    for chunk_idx in range(num_chunks-1):
-        start=chunk_idx*window_step
-        end=chunk_idx*window_step+chunk_length
-        chunk=data[start:end]
+    for chunk_idx in range(num_chunks - 1):
+        start = chunk_idx * window_step
+        end = chunk_idx * window_step + chunk_length
+        chunk = data[start:end]
         cut_data.append(chunk)
 
-    last_chunk=data[-chunk_length:]
+    last_chunk = data[-chunk_length:]
     cut_data.append(last_chunk)
 
     return cut_data
 
 
+def extract_polynomial_coefficients_from_2d_window(window: np.ndarray, order: int):
+    # TODO: add description
+    coefficients = []
+    x = np.linspace(0, 1, window.shape[0])
+    for feature_idx in range(window.shape[1]):
+        y = window[:, feature_idx]
+        coeffs = polynomial.polyfit(x, y, deg=order)[order]
+        coefficients.append(coeffs)
+    coefficients = np.array(coefficients)
+    return coefficients
 
 
+def extract_statistics_from_2d_window(window: np.ndarray,
+                                      statistic_types: Tuple[str, ...] = (
+                                      'mean', 'std', 'poly_1d', 'poly_2d')) -> np.ndarray:
+    """Extracts from 2d window functionals for each feature across instances (rows).
+    Currently supported functionals: ('mean', 'std')
+
+    :param window: np.ndarray
+                data in 2-d format (n_samples, n_features)
+    :param statistic_types: Tuple[str,...]
+                required functionals to extract.
+    :return: np.ndarray
+                extracted functionals with shape (n_samples, n_functionals)
+    """
+    supported_statistics = ('mean', 'std', 'poly_1d', 'poly_2d')
+    # check if statistic_types contains only supported functional types
+    if not set(statistic_types).issubset(supported_statistics):
+        raise AttributeError('Currently supported statistics:%s. Got %s.' % (supported_statistics, statistic_types))
+    if len(window.shape) != 2:
+        raise AttributeError('The window should be 2-dimensional. Got %i dimensions.' % (len(window.shape)))
+
+    result_statistics = []
+    for statistic_type in statistic_types:
+        if statistic_type == 'mean':
+            statistics = window.mean(axis=0)
+        elif statistic_type == 'std':
+            statistics = window.std(axis=0)
+        elif statistic_type == 'poly_1d':
+            statistics = extract_polynomial_coefficients_from_2d_window(window, order=1)
+        elif statistic_type == 'poly_2d':
+            statistics = extract_polynomial_coefficients_from_2d_window(window, order=2)
+        result_statistics.append(statistics)
+    result_statistics = np.concatenate(result_statistics, axis=0)
+    result_statistics = result_statistics[..., np.newaxis]
+    return result_statistics
 
 
-if __name__=='__main__':
-    path=r'E:\Databases\SEWA\Original\audio\SEW1101.wav'
-    sr, data=load_wav_file(path)
-    extracted_LLDs=cut_data_on_chunks(data,48000, 24000)
+if __name__ == '__main__':
+    path = r'E:\Databases\SEWA\Original\audio\SEW1101.wav'
+    sr, data = load_wav_file(path)
+    extracted_LLDs = cut_data_on_chunks(data, 48000, 24000)
     print(extracted_LLDs)
-    extracted_HLDs=extract_subwindow_EGEMAPS_from_audio_sequence(extracted_LLDs[0],sr, 0.1, 0.05)
-
+    extracted_HLDs = extract_subwindow_EGEMAPS_from_audio_sequence(extracted_LLDs[0], sr, 0.1, 0.05)

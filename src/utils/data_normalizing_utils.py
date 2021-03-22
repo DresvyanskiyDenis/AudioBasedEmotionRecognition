bd073720ff9846259b802d025bfa9c143c3c2b52
Denis <ecodenis@yandex.ru>
2021-03-22 22:08:53 +0100

    Functions for z-, power- and l2-normalizations were added.

--- src/utils/data_normalizing_utils.py
+++ src/utils/data_normalizing_utils.py
@@ -8,11 +8,11 @@ List of functions:
     * transform_data_with_scaler - transforms supplied data with provided scaler
     * normalize_min_max_data - normalizes/transforms supplied data using MinMax normalization and provides trained
     scaler, if it is needed.
+    # TODO: add new functions to description
 """
 from typing import Tuple, Optional
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.preprocessing import MinMaxScaler, PowerTransformer, normalize, StandardScaler
 
 __author__ = "Denis Dresvyanskiy"
 __copyright__ = "Copyright 2021"
@@ -65,6 +65,93 @@ def normalize_min_max_data(data:np.ndarray, return_scaler:bool=False) -> Tuple[n
         return transformed_data, normalizer
     else:
         return transformed_data
+
+def z_normalization(data:np.ndarray, return_scaler:bool=False,
+                    scaler:Optional[object]=None)->Union[np.ndarray, Tuple[np.ndarray, object]]:
+    """Normalizes data with provided normalizer, or generates new one and fit it.
+    Uses sklearn.preprocessing.StandardScaler() object.
+
+    :param data: np.ndarray
+                data in 2d-format (n_samples, n_features) or 1d-format (n_samples,)
+    :param return_scaler: bool
+                Should function return fitted scaler or not.
+    :param scaler: object (sklearn.preprocessing.StandardScaler)
+                If not None, the provided scaler will be used as normalizer.
+    :return: np.ndarray or (np.ndarray, scaler)
+                If return_scaler==False, returns normalized data
+                else returns normalized data and fitted scaler
+    """
+    # check if data is in appropriate format
+    if len(data.shape)>2:
+        raise AttributeError('The supplied data should be 1- or 2-dimensional. Got %i.'%(len(data.shape)))
+    # if data is 1-dimensional, it should be converted into 2-dimensional by adding additional dimension
+    if len(data.shape)==1:
+        data=data[..., np.newaxis]
+
+    # if no scaler supplied, create scaler and fit it
+    if scaler is None:
+        scaler=StandardScaler()
+        scaler.fit(data)
+    # transform data
+    data=scaler.transform(data)
+    # return scaler if need
+    if return_scaler:
+        return data, scaler
+    return data
+
+def l2_normalization(data:np.ndarray)->np.ndarray:
+    """Normalizes data with l2 normalization. Each instance (vector) will be normalized independently.
+        Uses sklearn.preprocessing.normalize() function.
+
+    :param data: np.ndarray
+                data in 2d-format (n_samples, n_features) or 1d-format (n_samples,)
+    :return: np.ndarray
+                normalized data
+    """
+    # check if data is in appropriate format
+    if len(data.shape) > 2:
+        raise AttributeError('The supplied data should be 1- or 2-dimensional. Got %i.' % (len(data.shape)))
+    # if data is 1-dimensional, it should be converted into 2-dimensional by adding additional dimension
+    if len(data.shape) == 1:
+        data = data[..., np.newaxis]
+    # normalize data. axis=1 means that each instance (row) will be independently normalized.
+    data=normalize(data, axis=1)
+    return data
+
+
+def power_normalization(data:np.ndarray, return_scaler:bool=False,
+                        scaler:Optional[object]=None)->Union[np.ndarray, Tuple[np.ndarray, object]]:
+    """Normalizes provided data via power normalization.
+    More: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PowerTransformer.html
+    Uses sklearn.preprocessing.PowerTransformer() object.
+    :param data: np.ndarray
+                data in 2d-format (n_samples, n_features) or 1d-format (n_samples,)
+    :param return_scaler: bool
+                Should function return fitted scaler or not.
+    :param scaler: object (sklearn.preprocessing.StandardScaler)
+                If not None, the provided scaler will be used as normalizer.
+    :return: np.ndarray or (np.ndarray, scaler)
+                If return_scaler==False, returns normalized data
+                else returns normalized data and fitted scaler
+    """
+    # check if data is in appropriate format
+    if len(data.shape) > 2:
+        raise AttributeError('The supplied data should be 1- or 2-dimensional. Got %i.' % (len(data.shape)))
+    # if data is 1-dimensional, it should be converted into 2-dimensional by adding additional dimension
+    if len(data.shape) == 1:
+        data = data[..., np.newaxis]
+
+    # if no scaler supplied, create scaler and fit it
+    if scaler is None:
+        scaler = PowerTransformer()
+        scaler.fit(data)
+    # transform data
+    data = scaler.transform(data)
+    # return scaler if need
+    if return_scaler:
+        return data, scaler
+    return data
+
 
 
 
